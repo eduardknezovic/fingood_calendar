be5a98311b221e1df1c47b7fb82c9eb01ee3ab6e
Eduard Knezovic <knezovic.eduard@gmail.com>
2021-02-14 22:04:25 +0100

    edit form - add error messages

--- pages/index.vue
+++ pages/index.vue
@@ -12,13 +12,67 @@
       </h1>
 
       <div>
-        <div class="error" v-if="!$v.DrawFrom.required">Field is required</div>
-        <b-datepicker v-model="form['DrawFrom']"></b-datepicker>
-        <b-datepicker v-model="form['FirstDate']"></b-datepicker>
-        <b-form-input v-model="form['Debt']" type="number" placeholder="Enter Debt"></b-form-input>
-        <b-form-input v-model="form['InterestRatePrc']" type="number" placeholder="Enter Interest Rate Percentage"></b-form-input>
-        <b-form-input v-model="form['PaymentCount']" type="number" placeholder="Enter Payment Count"></b-form-input>
-        <b-form-input v-model="form['PaymentPeriod']" type="number" placeholder="Enter Payment Period"></b-form-input>
+
+
+        <b-form-group label="Draw From">
+          <b-form-datepicker :state="validateState('DrawFrom')" v-model="form['DrawFrom']"></b-form-datepicker>
+          <b-form-invalid-feedback  v-if="!$v.form.DrawFrom.required">
+            This is a required field!
+          </b-form-invalid-feedback>
+          <b-form-invalid-feedback  v-if="!$v.form.FirstDate.laterDateThanDrawFrom">
+            This date must be before the "First Date" date
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+
+
+        <b-form-group label="First Date">
+          <b-datepicker :state="validateState('FirstDate')"  v-model="form['FirstDate']"></b-datepicker>
+          <b-form-invalid-feedback  v-if="!$v.form.FirstDate.required">
+            This is a required field!
+          </b-form-invalid-feedback>
+          <b-form-invalid-feedback  v-if="!$v.form.FirstDate.laterDateThanDrawFrom">
+            This date must be after the "Draw From" date
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+
+
+
+        <b-form-group label="Debt">
+            <b-form-input :state="validateState('Debt')"  v-model="form['Debt']" type="number" placeholder="Enter Debt"></b-form-input>
+          <b-form-invalid-feedback  v-if="!$v.form.Debt.required">
+            This is a required field!
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+
+
+        <b-form-group label="Interest Rate">
+            <b-form-input :state="validateState('InterestRatePrc')"  v-model="form['InterestRatePrc']" type="number" placeholder="Enter Interest Rate Percentage"></b-form-input>
+          <b-form-invalid-feedback  v-if="!$v.form.InterestRatePrc.required">
+            This is a required field!
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+
+
+        <b-form-group label="Payment Count">
+          <b-form-input :state="validateState('PaymentCount')"  v-model="form['PaymentCount']" type="number" placeholder="Enter Payment Count"></b-form-input>
+          <b-form-invalid-feedback  v-if="!$v.form.PaymentCount.required">
+            This is a required field!
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+
+        <b-form-group label="Payment Period">
+          <b-form-input :state="validateState('PaymentPeriod')"  v-model="form['PaymentPeriod']" type="number" placeholder="Enter Payment Period"></b-form-input>
+          <b-form-invalid-feedback  v-if="!$v.form.PaymentPeriod.required">
+            This is a required field!
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+
         <b-button @click="getTableRows()">Get Table</b-button>
       </div>
     </div>
@@ -39,7 +93,28 @@
 <script>
 import CalendarInput from '~/components/CalendarInput.vue'
 import CalendarTable from '~/components/CalendarTable.vue'
-import { required } from 'vuelidate/lib/validators'
+import { validationMixin } from "vuelidate";
+import { required, numeric, integer, between } from 'vuelidate/lib/validators'
+
+
+// custom validators
+function smallerOrEqualThanPaymentCount(value) {
+  return value <= this.form.PaymentCount
+}
+
+function divisibleByThree(value) {
+  return !(value % 3)
+}
+
+function divisibleByThreeOrIsEqualToOne(value) {
+  return divisibleByThree(value) || parseInt(value) === 1
+}
+
+function laterDateThanDrawFrom(value) {
+  console.log("HELLO later")
+  console.log(this.form.DrawFrom)
+  return value > this.form.DrawFrom || !!!this.form.DrawFrom
+}
 
 export default {
   components: {
@@ -51,36 +126,60 @@ export default {
       form: {
         DrawFrom: new Date(this.$route.query.drawFrom),
         FirstDate: new Date(this.$route.query.firstDate),
-        Debt: this.$route.query.debt,
-        InterestRatePrc: this.$route.query.interest,
-        PaymentCount: this.$route.query.paymentCount,
-        PaymentPeriod: this.$route.query.paymentPeriod,
+        Debt: Number(this.$route.query.debt),
+        InterestRatePrc: Number(this.$route.query.interest),
+        PaymentCount: parseInt(this.$route.query.paymentCount),
+        PaymentPeriod: parseInt(this.$route.query.paymentPeriod)
       },
       rows: [],
     }
   },
   validations: {
-    DrawFrom: {
-      required
-    },
-    FirstDate: {
-      required
-    },
-    Debt: {
-      required
-    },
-    InterestRatePrc: {
-      required
-    },
-    PaymentCount: {
-      required
-    },
-    PaymentPeriod: {
-      required
-    },
+    form: {
+      DrawFrom: {
+        required
+      },
+      FirstDate: {
+        required,
+        laterDateThanDrawFrom
+      },
+      Debt: {
+        required,
+        numeric,
+        between: between(0, 10000000000)
+      },
+      InterestRatePrc: {
+        required,
+        numeric,
+        between: between(0, 60)
+      },
+      PaymentCount: {
+        required,
+        integer,
+        divisibleByThree,
+        between: between(6, 300)
+      },
+      PaymentPeriod: {
+        required,
+        integer,
+        between: between(1, 300),
+        divisibleByThreeOrIsEqualToOne,
+        smallerOrEqualThanPaymentCount
+      },
+    }
   },
   methods: {
+    validateState(name) {
+      const { $dirty, $error } = this.$v.form[name];
+      return $dirty ? !$error : null;
+    },
     getTableRows() {
+      this.$v.$touch()
+      const isInvalid = this.$v.$invalid
+      if (isInvalid) {
+        this.rows = []
+        return
+      }
       // var url = "https://be-staging.fingood.cz:55400/api/common/SimulateCalendar"
       const PATH_API = "/api/common/SimulateCalendar"
       var url = `/api/v1${PATH_API}`
@@ -88,13 +187,28 @@ export default {
       const response = this.$axios.post(url, this.form).then(response => {
         this.rows = response.data["Rows"]
       })
+    },
+    loadDataFromRouteQuery() {
+      const query = this.$route.query
+      console.log("Hello!")
+      console.log(query)
+      if (!Object.keys(query).length) {
+        return true
+      }
+      /*
+      How can we do this?
+      We can copy paste from data to here and init data with empty strings.
+      I wonder how would that look like...
+
+      It is important that we are giving the variables the same thing that the inputs are giving!
+      Almost crucial.
+       */
     }
   },
   computed: {
     items() {
       var formatted
       formatted = this.rows.map(row => {
-
         const formatter = new Intl.NumberFormat('cs', {
           style: 'currency',
           currency: 'CZK',
@@ -103,39 +217,21 @@ export default {
 
         delete row["Number"]
         row["Date"] = new Date(row["Date"]).toLocaleDateString('cs')
-        //row["Date"] = "1.1.2020"
         row["PaidDate"] = new Date(row["PaidDate"]).toLocaleDateString('cs')
-        // row["PaidDate"] = "2.2.2020"
         row["Amount"] = formatter.format(row["Amount"])
         row["Interest"] = formatter.format(row["Interest"])
         row["Debt"] = formatter.format(row["Debt"])
-        // row["PaidAmount"] = 20000000
         row["PaidAmount"] = formatter.format(row["PaidAmount"])
         row["Principal"] = formatter.format(row["Principal"])
         return row
       })
 
-      /*
-      Use this to test long outputs?
-      formatted.push.apply(formatted, formatted)
-      formatted.push.apply(formatted, formatted)
-      */
-
-      /*
-      What do we need to do here?
-      - Remove the number ?
-      - Format dates to cz
-      - Format money to cz
-       */
-
       return formatted
     }
   },
   beforeMount() {
-    // check if all of the values are present needed to make the request
-    // if yes, make the request!
-    var someValueMissing = Object.keys(this.form).some(key => this.form[key] === undefined)
-    if (someValueMissing) {
+    var isQueryEmpty = this.loadDataFromRouteQuery()
+    if (isQueryEmpty) { // so the validator errors will not get triggered!
       return
     }
     this.getTableRows()
