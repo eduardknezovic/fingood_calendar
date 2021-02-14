905a4bbf62b5bfebf8bd06d318fd5c8c370dd15b
Eduard Knezovic <knezovic.eduard@gmail.com>
2021-02-14 18:34:33 +0100

    install vuelidate

--- pages/index.vue
+++ pages/index.vue
@@ -12,12 +12,13 @@
       </h1>
 
       <div>
-        <b-datepicker v-model="formInput['DrawFrom']"></b-datepicker>
-        <b-datepicker v-model="formInput['FirstDate']"></b-datepicker>
-        <b-form-input v-model="formInput['Debt']" type="number" placeholder="Enter Debt"></b-form-input>
-        <b-form-input v-model="formInput['InterestRatePrc']" type="number" placeholder="Enter Interest Rate Percentage"></b-form-input>
-        <b-form-input v-model="formInput['PaymentCount']" type="number" placeholder="Enter Payment Count"></b-form-input>
-        <b-form-input v-model="formInput['PaymentPeriod']" type="number" placeholder="Enter Payment Period"></b-form-input>
+        <div class="error" v-if="!$v.DrawFrom.required">Field is required</div>
+        <b-datepicker v-model="form['DrawFrom']"></b-datepicker>
+        <b-datepicker v-model="form['FirstDate']"></b-datepicker>
+        <b-form-input v-model="form['Debt']" type="number" placeholder="Enter Debt"></b-form-input>
+        <b-form-input v-model="form['InterestRatePrc']" type="number" placeholder="Enter Interest Rate Percentage"></b-form-input>
+        <b-form-input v-model="form['PaymentCount']" type="number" placeholder="Enter Payment Count"></b-form-input>
+        <b-form-input v-model="form['PaymentPeriod']" type="number" placeholder="Enter Payment Period"></b-form-input>
         <b-button @click="getTableRows()">Get Table</b-button>
       </div>
     </div>
@@ -38,6 +39,7 @@
 <script>
 import CalendarInput from '~/components/CalendarInput.vue'
 import CalendarTable from '~/components/CalendarTable.vue'
+import { required } from 'vuelidate/lib/validators'
 
 export default {
   components: {
@@ -46,238 +48,44 @@ export default {
   },
   data() {
     return {
-      formInput: {
-        "DrawFrom": new Date(this.$route.query.drawFrom),
-        "FirstDate": new Date(this.$route.query.firstDate),
-        "Debt": this.$route.query.debt,
-        "InterestRatePrc": this.$route.query.interest,
-        "PaymentCount": this.$route.query.paymentCount,
-        "PaymentPeriod": this.$route.query.paymentPeriod,
+      form: {
+        DrawFrom: new Date(this.$route.query.drawFrom),
+        FirstDate: new Date(this.$route.query.firstDate),
+        Debt: this.$route.query.debt,
+        InterestRatePrc: this.$route.query.interest,
+        PaymentCount: this.$route.query.paymentCount,
+        PaymentPeriod: this.$route.query.paymentPeriod,
       },
       rows: [],
-      test_rows: [
-        {
-            "Number": 1,
-            "Date": "2020-03-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 816666.67,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 816666.67
-        },
-        {
-            "Number": 2,
-            "Date": "2020-04-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 3,
-            "Date": "2020-05-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 4,
-            "Date": "2020-06-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 5,
-            "Date": "2020-07-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 6,
-            "Date": "2020-08-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 7,
-            "Date": "2020-09-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 8,
-            "Date": "2020-10-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 9,
-            "Date": "2020-11-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 10,
-            "Date": "2020-12-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 11,
-            "Date": "2021-01-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 12,
-            "Date": "2021-02-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 13,
-            "Date": "2021-03-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 14,
-            "Date": "2021-04-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 15,
-            "Date": "2021-05-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 16,
-            "Date": "2021-06-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 17,
-            "Date": "2021-07-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 18,
-            "Date": "2021-08-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 19,
-            "Date": "2021-09-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 20,
-            "Date": "2021-10-10T11:39:33.912Z",
-            "Principal": 0.0,
-            "Interest": 500000.00,
-            "Debt": 50000000.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 500000.00
-        },
-        {
-            "Number": 21,
-            "Date": "2021-11-10T11:39:33.912Z",
-            "Principal": 50000000.0,
-            "Interest": 500000.00,
-            "Debt": 0.0,
-            "PaidAmount": 0.0,
-            "PaidDate": "1900-01-01T00:00:00",
-            "Amount": 50500000.00
-        }
-    ]
     }
+  },
+  validations: {
+    DrawFrom: {
+      required
+    },
+    FirstDate: {
+      required
+    },
+    Debt: {
+      required
+    },
+    InterestRatePrc: {
+      required
+    },
+    PaymentCount: {
+      required
+    },
+    PaymentPeriod: {
+      required
+    },
   },
   methods: {
     getTableRows() {
-      console.log("Hello!")
-      console.log(this.formInput)
-      var url = "https://be-staging.fingood.cz:55400/api/common/SimulateCalendar"
+      // var url = "https://be-staging.fingood.cz:55400/api/common/SimulateCalendar"
       const PATH_API = "/api/common/SimulateCalendar"
-      url = `/api/v1${PATH_API}`
+      var url = `/api/v1${PATH_API}`
       // const url = "/api"
-      const response = this.$axios.post(url, this.formInput).then(response => {
+      const response = this.$axios.post(url, this.form).then(response => {
         this.rows = response.data["Rows"]
       })
     }
@@ -324,11 +132,9 @@ export default {
     }
   },
   beforeMount() {
-    console.log(this.formInput["Debt"])
     // check if all of the values are present needed to make the request
     // if yes, make the request!
-    console.warn(this.formInput["PaymentPeriod"])
-    var someValueMissing = Object.keys(this.formInput).some(key => this.formInput[key] === undefined)
+    var someValueMissing = Object.keys(this.form).some(key => this.form[key] === undefined)
     if (someValueMissing) {
       return
     }
